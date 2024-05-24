8135cb01b835caaaefc84981389b70f5f5977a83
Mathilde Po <mathilde@MacBook-Pro-de-Mathilde-2.local>
2024-05-24 09:12:33 +0200

    m

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f6c1efe7e5cfd7abcea430a9d2efa84599f3a5bc',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f6c1efe7e5cfd7abcea430a9d2efa84599f3a5bc',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
